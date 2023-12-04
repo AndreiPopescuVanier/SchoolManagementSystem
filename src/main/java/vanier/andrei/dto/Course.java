131d46b218a9c0dbda85314ae680c091d71b6b85
Andrei <ierdna100@yahoo.com>
2023-12-03 20:08:40 -0500

    Implemented Course

--- src/main/java/vanier/andrei/dto/Course.java
+++ src/main/java/vanier/andrei/dto/Course.java
@@ -15,10 +15,25 @@ public class Course {
 
     private Department department;
     @Getter @Setter private Teacher teacher;
-    @Getter @Setter private Student[] students;
+    @Getter private Student[] students;
     @Getter private int studentCount;
 
     public Course(String name, double credit, Department department) {
+        this.courseName = name;
+        this.credit = credit;
+        this.department = department;
+        this.id = "C" + String.format("%03d", nextId++);
+    }
 
+    public void setStudents(Student[] students) {
+        this.students = students;
+
+        for (int i = 0; i < students.length; i++) {
+            // Because our array is gap-less, first null indicates end
+            if (students[i] == null) {
+                studentCount = i + 1;
+                return;
+            }
+        }
     }
 }
