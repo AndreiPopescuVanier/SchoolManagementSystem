b6e72c86cbc8882ec883d137444ef0a9b27e3fa6
Andrei <6239045@edu.vaniercollege.qc.ca>
2023-12-03 20:23:30 -0500

    Fixed github remote account

--- src/main/java/vanier/andrei/Main.java
+++ src/main/java/vanier/andrei/Main.java
@@ -4,8 +4,12 @@ package vanier.andrei;
 // 5.n.iv:  If the course has be registered by the student already, it cannot be registered again, print a message.
 // 9.f: Student register course failed, student has registered that course
 
+import vanier.andrei.dto.SchoolManagementSystem;
+
 public class Main {
     public static void main(String[] args) {
+        SchoolManagementSystem sms = new SchoolManagementSystem();
 
+        
     }
 }
